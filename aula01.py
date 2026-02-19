763a2ef3103e5d6966c7da3a8e506c7420207a57
Emily Costa Araujo <emilyccostaaraujo71@gmail.com>
2026-02-19 10:04:32 -0300

    Primeira aula de programação

---
+++ aula01.py
@@ -0,0 +1,9 @@
+idade = 15 # variável inteira
+peso = 42.7 # variável float
+nome = "Emily Costa Araújo" # variável string
+sexo_feminino = True # variável bool
+
+print(nome)
+print(idade)
+print(peso)
+print(sexo_feminino)
