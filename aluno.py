e3e8c1b243c673e4aad17075ff754ddcd6003dd7
Emily <emilycostaaraujo71@gmail.com>
2026-02-20 10:02:55 -0300

    Aula do dia vinte

---
+++ aluno.py
@@ -0,0 +1,13 @@
+nome = input("digite seu nome: ")
+telefone = input("digite seu telefone: ")
+idade = input("digite sua idade: ")
+cidade = input("digite sua cidade: ")
+
+print("ola",nome)
+print("seu telefone é",telefone)
+print("sua idade é",idade)
+print("sua cidade é:",cidade)
+
+
+
+
---
+++ matematica.py
@@ -0,0 +1,12 @@
+valor_inteiro1 = int(input("digite o primeiro inteiro: "))
+valor_inteiro2 = int(input("digite o segundo inteiro: "))
+
+soma = valor_inteiro1 + valor_inteiro2
+subtracao = valor_inteiro1 - valor_inteiro2
+divisao = valor_inteiro1 / valor_inteiro2
+multiplicacao = valor_inteiro1 * valor_inteiro2
+
+print("o resultado da soma é:", soma )
+print("o resultado da subtracao é:", subtracao)
+print("o resultado da divisao é:", divisao)
+print("o resultado da multiplicacao é:", multiplicacao)
