1a9c48be83c3fb3830ed492ddbcf4ae346647ff4
emily <emilycostaaraujo71@gmail.com>
2026-02-24 11:47:22 -0300

    aula do dia

---
+++ exercicio03.py
@@ -0,0 +1,9 @@
+nome = input("digite seu nome: ")
+dolar = 5.00
+quantos_reais_voce_tem = float(input("digite o seu valor valor em real: "))
+
+divisao = quantos_reais_voce_tem / dolar
+
+print ("ola", nome)
+print ("a quantidade de dolar é:", divisao)
+
