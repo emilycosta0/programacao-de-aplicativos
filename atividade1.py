1bc57f4c09f6ced72ac857c72a4a13c7d318f674
emily costa <emilycostaaraujo71@gmail.com>
2026-03-05 10:10:42 -0300

    Aula do dia 05/03

---
+++ atividade1.py
@@ -0,0 +1,13 @@
+comprimento = input("O comprimento da peça esta entre 10cm a 12cm? (S/N):  ")
+if comprimento == "sim":
+    largura = print("A largura esta entre 5cm e 6cm? (S/N): ")
+    if largura == "S":  
+        print("Peça aprovada!")
+        else:
+            print("Reprovado! Peça invalida.")
+else:
+    print("reprovado! Peça invalida.")
+
+
+
+
