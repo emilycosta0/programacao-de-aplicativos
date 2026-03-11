8d6bd618f89be8ba8001297e91322a49ff59604e
Emily Costa <emilycostaaraujo71@gmail.com>
2026-03-11 12:00:11 -0300

    Aula do dia 11/03

--- distribuição.py
+++ distribuição.py
@@ -5,4 +5,7 @@ if peso < 5 and (codigo % 10 == 0):
     print(f"Pacote {codigo}: ENTREGA EXPRESSA")
 elif peso > 50:
     print(f"Pacote {codigo}: CARGA PESADA")
-.
+else:
+    print("Informações inválidas.")
+    
+
