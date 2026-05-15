1f7e041ac0b854e4bab22431d6b8fc22a8eba47f
Emily Costa <emilycostaaraujo71@gmail.com>
2026-05-15 08:11:15 -0300

    aula do dia 15/05

--- desafio-arquivo2.py
+++ desafio-arquivo2.py
@@ -9,7 +9,7 @@ def mudar_habitos():
 
 def ler():
     with open('habitos.txt', 'r') as arquivo:
-        habitos = arquivo.readlines()
+        habitos = arquivo.readlines() 
 
     h = 0
 
@@ -20,3 +20,39 @@ def ler():
 def atualizar():
     ler()
     alterar_habitos = int(input("Digite os hábitos que deseja alterar: "))
+    novo_habito = input("Novo hábito: ")
+
+    with open('habitos.txt','r') as arquivo:
+        linhas = arquivo.readlines()
+
+    linhas[alterar_habitos] = novo_habito + '\n'
+
+    with open('habitos.txt', 'w') as arquivo:
+        arquivo.writelines(linhas)
+        print("Hábitos atualizados!")
+
+
+def deletar():
+    ler()
+    alterar_habitos = int(input("Digite o hábito que deseja excluir: "))
+
+    with open('habitos.txt', 'r') as arquivo:
+        linhas = arquivo.readlines()
+
+    del linhas [alterar_habitos]
+
+    with open('habitos.txt', 'w') as arquivo:
+        arquivo.writelines(linhas)
+    print("Hábito removido!")
+
+while True:
+    print("\n1-Adicionar | 2-Ver Todos | 3-Editar | 4-Excluir | 5-Sair" )
+    opcao = input("Escolha uma das opções acima: ")
+
+    if opcao == "1": mudar_habitos()
+    elif opcao == "2": ler()
+    elif opcao == "3": atualizar()
+    elif opcao == "4": deletar()
+    elif opcao == "5": break
+
+
