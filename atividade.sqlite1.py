111a51487e90c62adeacf0e315ab330ee00f34f7
Emily Costa <emilycostaaraujo71@gmail.com>
2026-06-12 10:11:43 -0300

    Aula do dia 12/06

--- atividade.sqlite1.py
+++ atividade.sqlite1.py
@@ -17,7 +17,7 @@ cursor.execute('''
 
 
 nome_aluno = input("Digite o nome do aluno: ")
-telefone_aluno = input("Dgite o telefone do aluno: ")
+telefone_aluno = input("Digite o telefone do aluno: ")
 turma_aluno = input("Digite a turma do aluno: ")
 idade_aluno = int(input("Digite a idade do aluno: "))
 cpf_aluno = input("Digite o cpf do aluno: ")
@@ -28,4 +28,51 @@ comando_inserir = f'''
 
 cursor.execute(comando_inserir)
 conexao.commit()
+
+cursor.execute('''SELECT * FROM alunos''')
+todos_alunos = cursor.fetchall()
+
+if not todos_alunos:
+    print("Nenhum aluno cadastrado.")
+else:
+    for aluno in todos_alunos:
+        print(f"ID: {aluno[0]}, Nome: {aluno[1]}, Telefone: {aluno[2]}, Turma: {aluno[3]}, Idade: {aluno[4]}, CPF: {aluno[5]} ")
 conexao.close()
+
+
+
+def atualizar_dados_aluno():
+    conn = sqlite3.connect('escola_demonstracao.db')
+    cursor = conn.cursor()
+    
+    id_aluno = 1
+    novo_nome = "Gabriela Ropelatto"
+    novo_cpf = "111.222.333-44"
+
+    cursor.execute("SELECT * FROM Alunos WHERE id = ?", (id_aluno,))
+    aluno_antigo = cursor.fetchone()
+    
+    if aluno_antigo:
+        print(f"Aluno encontrado antes da atualização: {aluno_antigo}")
+        
+        sql_atualiza = """
+            UPDATE Alunos 
+            SET nome = ?, cpf = ? 
+            WHERE id = ?
+        """
+        cursor.execute(sql_atualiza, (novo_nome, novo_cpf, id_aluno))
+        
+        conn.commit()
+        print("Dados alterados com sucesso!")
+        
+        cursor.execute("SELECT * FROM Alunos WHERE id = ?", (id_aluno,))
+        aluno_atualizado = cursor.fetchone()
+        print(f"Aluno após a atualização: {aluno_atualizado}")
+    else:
+        print("Aluno não encontrado com o ID especificado.")
+
+    
+    conn.close()
+
+if __name__ == "__main__":
+    atualizar_dados_aluno()
